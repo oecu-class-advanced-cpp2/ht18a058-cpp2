fe043df30bf953d6d281526f5393c804d02d54af
ht18a058 <ht18a058@oecu.jp>
2020-12-12 18:35:59 +0900

    ex-1-main.cppにnth_prime_testを追加

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -29,6 +29,14 @@ int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
 		return 0;
 }
 
+void nth_prime_test(unsigned int ans, unsigned int a, unsigned int d, unsigned int n) {
+	if (ans == nth_prime(a, d, n)) {
+		std::cout << "o—ÍŒ‹‰Ê‚Æ³‰ğ‚Íˆê’v‚µ‚Ü‚µ‚½" << std::endl;
+	} else {
+		std::cout << "o—ÍŒ‹‰Ê‚Æ³‰ğ‚Íˆê’v‚µ‚Ü‚¹‚ñ‚Å‚µ‚½" << std::endl;
+	}
+}
+
 int main() {
 	std::cout << nth_prime(367, 186, 151) << std::endl;
 	std::cout << nth_prime(179, 10, 203) << std::endl;
@@ -42,6 +50,9 @@ int main() {
 	std::cout << nth_prime(331, 221, 177) << std::endl;
 	std::cout << nth_prime(259, 170, 40) << std::endl;
 	std::cout << nth_prime(269, 58, 102) << std::endl;
+
+	nth_prime_test(92808, 367, 186, 151);
+	nth_prime_test(92809, 367, 186, 151);
 
 	int hoge;
 	std::cin >> hoge;
